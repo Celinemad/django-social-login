4c5369c1c0039c69b6068f8c9d579a2f0b3261f6
Celinemad <rk243812@gmail.com>
2023-07-24 15:21:51 +0900

    DRF로 JWT 기반 Authentication 세팅

---
+++ django_social_login/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('dj/', include('dj_rest_auth.urls')),
+    path('dj/registration/', include('dj_rest_auth.registration.urls')),
+    # path('accounts/', include('allauth.urls')),
+    # path('accounts/', include('accounts.urls')),
+]
